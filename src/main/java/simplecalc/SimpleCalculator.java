2c3933c9a5e9256b0a76cbac7ade2ca9c87736fb
이호준 <@DESKTOP-FCT7T0B>
2016-04-11 17:15:37 +0900

    곱셈 기능 테스트 코드 추가
    본 기능

--- src/main/java/simplecalc/SimpleCalculator.java
+++ src/main/java/simplecalc/SimpleCalculator.java
@@ -8,6 +8,9 @@ public class SimpleCalculator {
 	public void sub(int x, int y){
 		result = x-y;
 	}
+	public void multi(int x, int y){
+		result = x*y;
+	}
 	public int getResult(){
 		return result;
 	}
--- src/test/java/simplecalc/SimpleCalculatorTest.java
+++ src/test/java/simplecalc/SimpleCalculatorTest.java
@@ -12,10 +12,17 @@ public class SimpleCalculatorTest {
 		calc.add(10, 20);
 		assertEquals(30,calc.getResult());
 		}
+	@Test
 	public void testsub() {
 		SimpleCalculator calc = new SimpleCalculator();
 		calc.sub(10, 20);
 		assertEquals(-10,calc.getResult());
+		}
+	@Test
+	public void testmulti() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.multi(10, 20);
+		assertEquals(200,calc.getResult());
 		}
 	
 	
